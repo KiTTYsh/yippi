aca6a02ba6db063d07bb9b4e029ec0af47f832cf
Rendy Arya Kemal <rendyarya22@gmail.com>
2018-03-30 14:17:19 +0700

    Release stuffs

---
+++ README.md
@@ -0,0 +1,18 @@
+#Yippi! 
+Yippi is an e621 API wrapper for Python, to make everyone's life simplier.
+
+#Requirements 
++ Python
++ urllib
++ json
+
+#Installation 
+Clone this repository, and type this down 
+```bash
+$ pip install -e .``` 
+
+#Example 
+```python
+results = yippi.search(['girly', 'male', 'fox']) # Searches e621 with "girly male fox" as query
+results.file_url # Get the image/file's URL``` 
+This is only a basic example, I'll add further documentation later.
---
+++ setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name = 'yippi',
+    packages = ['yippi'], # this must be the same as the name above
+    version = '0.0.1',
+    description = 'an e621 API wrapper for Python.',
+    long_description = open('README.md').read(),
+    license = 'MIT',
+    author = 'Rendy Arya Kemal',
+    author_email = 'rendyarya22@gmail.com',
+    url = 'https://github.com/Rendyindo/yippi', # use the URL to the github repo
+    classifiers = [
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords = ['yippi', 'e621', 'API'], # arbitrary keywords
+    install_requires = [
+    ],
+)
